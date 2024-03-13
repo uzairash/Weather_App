ca6fb9c5c2f0c8590b12afea0542ae50a9731285
uzairash <uzairasgfaq90@gmail.com>
2024-03-14 00:43:34 +0500

    adding version increment

--- scripts.groovy
+++ scripts.groovy
@@ -1,11 +1,35 @@
-def imageName = 'weather_app-v' 
-def imageVersion = readFile(file: 'version.txt')
+// def imageName = 'weather_app-v' 
+// def imageVersion = readFile(file: 'version.txt')
 
-def getVersion() {
-    echo 'script.groovy file....'
-    def (major, minor) = imageVersion.tokenize('.').collect { it.toInteger() }
-    minor++
-    imageVersion = "${major}.${minor}"
-    writeFile(file: versionFilePath, text: imageVersion) // Update version in the file
-    return "${imageName}${imageVersion}"
+// def getVersion() {
+//     echo 'script.groovy file....'
+//     def (major, minor) = imageVersion.tokenize('.').collect { it.toInteger() }
+//     minor++
+//     imageVersion = "${major}.${minor}"
+//     writeFile(file: versionFilePath, text: imageVersion) // Update version in the file
+//     return "${imageName}${imageVersion}"
+// }
+
+def test() {
+    echo "executing pipeline fron branch $BRANCH_NAME"
+    echo 'running tests'
 }
+def buildJar() {
+    echo 'Building the application ...'
+    sh 'mvn package'
+
+}
+
+def buildImage() {
+    echo 'Building the docker image...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable:'PASS', usernameVariable:'USER')]) {
+        sh 'docker build -t uzair102/u_repo:jma-2.1 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push uzair102/u_repo:jma-2.1'
+    }
+}
+
+def deploy() {
+      echo 'Deploying the application...'
+}
+return this
